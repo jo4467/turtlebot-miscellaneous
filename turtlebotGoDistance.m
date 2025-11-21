28db57a83bf3d2a7703348e2d4baafa84910fbcc
jo4467 <91754123+jo4467@users.noreply.github.com>
2025-11-21 17:35:41 -0500

    Update turtlebotGoDistance.m

--- turtlebotGoDistance.m
+++ turtlebotGoDistance.m
@@ -6,20 +6,6 @@ forwardSpeed = 0.3;
 %odometry
 odometryStatebegin = getTurtlebotOdometry(odometrySubscriber, 0);
 
-% we're just doing to do this by timing for now...
-% get the time to run forward (use 0.01s intervals for sending)
-% timeInterval = 0.01;
-% totalTime = abs(distance)/forwardSpeed;
-% n_timeSteps = floor(totalTime/timeInterval);
-
-% run at speed for given time
-% for i = 1:n_timeSteps
-%     % send velocity command
-%     forwardSpeed = sign(distance)*abs(forwardSpeed);
-%     turtlebotSendSpeed(forwardSpeed, 0, velocityPublisher)
-%     % pause for timing
-%     pause(timeInterval);
-% end
 difference = [0, 0];
 while sqrt(difference(1).^2 + difference(2).^2) < abs(distance)
     % send velocity command
@@ -30,3 +16,4 @@ while sqrt(difference(1).^2 + difference(2).^2) < abs(distance)
 end
 
 turtlebotStop(velocityPublisher);
+
